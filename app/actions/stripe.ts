756c4428f3b1c7d063ff89f78fb928ecd429bf7e
Sam Thompson <sam.thompson@hey.com>
2023-08-11 10:23:36 -0400

    clean-up

--- app/actions/stripe.ts
+++ app/actions/stripe.ts
@@ -5,8 +5,6 @@ import type { Stripe } from "stripe";
 import { redirect } from "next/navigation";
 import { headers } from "next/headers";
 
-import { CURRENCY } from "@/config";
-import { formatAmountForStripe } from "@/utils/stripe-helpers";
 import { stripe } from "@/lib/stripe";
 
 export async function createCheckoutSession(data: FormData): Promise<void> {
@@ -26,20 +24,4 @@ export async function createCheckoutSession(data: FormData): Promise<void> {
     });
 
   redirect(checkoutSession.url as string);
-}
-
-export async function createPaymentIntent(
-  data: FormData
-): Promise<{ client_secret: string }> {
-  const paymentIntent: Stripe.PaymentIntent =
-    await stripe.paymentIntents.create({
-      amount: formatAmountForStripe(
-        Number(data.get("customDonation") as string),
-        CURRENCY
-      ),
-      automatic_payment_methods: { enabled: true },
-      currency: CURRENCY,
-    });
-
-  return { client_secret: paymentIntent.client_secret as string };
 }
